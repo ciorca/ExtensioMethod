2915019a5d17accbdc1c115dd3e1dae81946938a
ciorca <ciorca.gaby@gmail.com>
2022-06-17 17:58:40 +0300

    Add project files.

---
+++ ExtensioMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensioMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> Numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            IEnumerable<int> EvenNumbers = Enumerable.Where(Numbers, n => n % 2 == 0);
+            foreach(int evenNumber in EvenNumbers)
+            {
+                Console.WriteLine(evenNumber);
+            }
+        }
+    }
+}
---
+++ ExtensioMethod/StringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensioMethod
+{
+    public static class StringHelper
+    {
+        //width "this" an "static" added to the class,became an extension Method
+        public static string ChangeFirstLetterCase(this string inputString)
+        {
+            if (inputString.Length > 0)
+            {
+                char[] charArray = inputString.ToCharArray();
+                charArray[0] = char.IsUpper(charArray[0]) ? char.ToLower(charArray[0]) : char.ToUpper(charArray[0]);
+                return new string(charArray);
+            }
+            return inputString;
+        }
+    }
+}
